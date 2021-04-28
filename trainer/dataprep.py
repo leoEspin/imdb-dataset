c98e2a75dd25251a7fd1f41aed0f143f80c9a87c
leo espin <35843183+leoEspin@users.noreply.github.com>
2021-04-28 12:45:24 -0400

    added and renamed vocabulary-size argument

--- trainer/dataprep.py
+++ trainer/dataprep.py
@@ -2,11 +2,11 @@ import tensorflow as tf
 import tensorflow.keras.datasets.imdb as imdb
 import re
 
-def get_and_pad_imdb_dataset(num_words=10000, maxlen=250, index_from=3):
+def get_and_pad_imdb_dataset(vocab_size=10000, maxlen=250, index_from=3):
     from tensorflow.keras.datasets import imdb
 
     # Load data with defaults https://www.tensorflow.org/api_docs/python/tf/keras/datasets/imdb/load_data
-    (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=num_words,
+    (x_train, y_train), (x_test, y_test) = imdb.load_data(vocab_size=vocab_size,
                                                           skip_top=0,
                                                           maxlen=maxlen,
                                                           start_char=1,
@@ -27,10 +27,10 @@ def get_and_pad_imdb_dataset(num_words=10000, maxlen=250, index_from=3):
     return (x_train, y_train), (x_test, y_test)
 
 
-def get_imdb_word_index(num_words=10000, index_from=3):
+def get_imdb_word_index(vocab_size=10000, index_from=3):
     imdb_word_index = tf.keras.datasets.imdb.get_word_index()
     imdb_word_index = {key: value + index_from for
-                       key, value in imdb_word_index.items() if value <= num_words-index_from}
+                       key, value in imdb_word_index.items() if value <= vocab_size-index_from}
     return imdb_word_index
 
 
--- trainer/main.py
+++ trainer/main.py
@@ -8,11 +8,13 @@ def parcero():
     parce.add_argument('--epochs', default=10, type=int)
     parce.add_argument('--batch-size', default=32, type=int)
     parce.add_argument('--max-seq-len', default=250, type=int) # limit review size to 250 words to reduce training time
+    parce.add_argument('--vocab-size', default=10000, type=int) # number of words in the model's vocabulary
     return parce.parse_args()
 
 def train_model():
     arguments = parcero()
-    (x_train, y_train), (x_test, y_test) = dataprep.get_and_pad_imdb_dataset(maxlen=arguments.max_seq_len)
+    (x_train, y_train), (x_test, y_test) = dataprep.get_and_pad_imdb_dataset(vocab_size=arguments.vocab_size,
+                                                                            maxlen=arguments.max_seq_len)
     imdb_word_index = dataprep.get_imdb_word_index()
     max_index_value = max(imdb_word_index.values()) 
 
